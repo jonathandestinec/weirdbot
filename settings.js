5756e629d56a918dd8d8667dbbdf719756465e1b
jonathandestinec <jonathandestinec@gmail.com>
2024-10-14 17:51:53 +0100

    done

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'W.E.I.R.D BOT 🤖',
+  author: 'lime-juice',
+  botName: "Orange",
+  botOwner: 'Lime-Juice', // Your name
+  ownerNumber: '+2349061426386', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
